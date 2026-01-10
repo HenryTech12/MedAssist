d1df701eb2a0d4c5b06660ef43c64fdc5ed66c14
HenryTech12 <fakorodehenry@gmail.com>
2026-01-10 11:21:27 +0100

    resolved repository issue

--- src/main/java/com/medassist/entity/Appointment.java
+++ src/main/java/com/medassist/entity/Appointment.java
@@ -1,5 +1,8 @@
 package com.medassist.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -8,9 +11,11 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @Data
+@Entity
 public class Appointment {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDate day;
     private LocalTime time;
